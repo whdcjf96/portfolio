672eb1f91ff15d7e05266596f262ac67096531f7
Yu Jong Chuel <aaaaaaa123455549@gmail.com>
2019-11-11 14:18:27 +0900

    프로젝트추가

---
+++ mobileserver/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MobileserverConfig(AppConfig):
+    name = 'mobileserver'
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mobileserver.apps.MobileserverConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
